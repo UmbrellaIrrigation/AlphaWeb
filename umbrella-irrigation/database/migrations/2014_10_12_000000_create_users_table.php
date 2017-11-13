7b6f76e819723e826a46e3cf4148dcdf52c16054
Rbilemjian <rbilemjian@gmail.com>
2017-11-13 11:18:12 -0800

    Revert "UUIDs for User Table"
    This reverts commit 905525b573a133b3decb4a96f60608ebefbf4515.

--- umbrella-irrigation/database/migrations/2014_10_12_000000_create_users_table.php
+++ umbrella-irrigation/database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+
 class CreateUsersTable extends Migration
 {
     /**
@@ -10,12 +11,10 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->string('id');
-            $table->primary('id');
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
@@ -29,4 +28,8 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
 }
