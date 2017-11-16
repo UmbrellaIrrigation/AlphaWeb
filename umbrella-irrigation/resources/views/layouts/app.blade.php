f5ba2879b73cd72e9fd2792444316af2f840f28f
Rbilemjian <rbilemjian@gmail.com>
2017-11-16 09:27:20 -0800

    Adding Permission Level Display in Navbar

--- umbrella-irrigation/resources/views/layouts/app.blade.php
+++ umbrella-irrigation/resources/views/layouts/app.blade.php
@@ -36,6 +36,18 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
+                        @if(Auth::check())
+                            @if(Auth::user()->isAdmin())
+                                <li>Permission Level: <b>Admin</b></li>
+
+                            @elseif(Auth::user()->isEmployee())
+                                Permission Level: <b>Employee</b>
+
+                            @else
+                                Permission Level: <b>Guest</b>
+                            @endif
+                            </li>
+                        @endif
                         &nbsp;
                     </ul>
 
@@ -49,6 +61,8 @@
                             @if(Auth::user()->isAdmin())
                                 <li><a href="{{ route('users') }}">Users</a></li>
                             @endif
+
+
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
