0e57c3220f16df9821beb5d50ebd25969bc51a93
Rbilemjian <rbilemjian@gmail.com>
2017-11-16 01:18:48 -0800

    Users List Page for Admins
    Users list which fetches all users except for currently logged in admin.
    Only works for admins. TBD: Allow admin to modify permissions of
    non-admin users.

--- umbrella-irrigation/resources/views/users.blade.php
+++ umbrella-irrigation/resources/views/users.blade.php
@@ -5,11 +5,36 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading"><center>Users</center></div>
 
                 <div class="panel-body">
+                    <table style="width:100%">
+                        <thead>
+                            <tr>
+                                <th><center>Name</center></th>
+                                <th><center>E-mail</center></th>
+                                <th><center>Permission</center></th>
+                            </tr>
+                        </thead>
+                        <tbody>
                     @foreach($users as $user)
-                    <li>{{$user->name}}</li>
+                        <tr>
+                            <td><center>{{$user->name}}</center></td>
+                            <td><center>{{$user->email}}</center></td>
+                            <td>
+                                <center>
+                                @if($user->isAdmin())
+                                Admin
+                                @elseif($user->isEmployee())
+                                Employee
+                                @else
+                                Guest
+                                @endif
+                                </center>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
                     @endforeach
                 </div>
             </div>
