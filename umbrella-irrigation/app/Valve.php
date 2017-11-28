f0b1b7e7a3e2589ebb5d092f731a082086eb48bc
Unknown <ordonez.ramses@gmail.com>
2017-11-28 14:11:48 -0800

    valve/valve group functions

--- umbrella-irrigation/app/Valve.php
+++ umbrella-irrigation/app/Valve.php
@@ -3,56 +3,81 @@
 namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Valve extends Model
 {
 	public static function boot()
     {
       parent::boot();
-      self::creating(function ($model) {
+      self::creating(function ($model){
         $model->id = (string) Uuid::generate(4);
         });
     }
     
 	public $incrementing = false;
 
-    public function isParent(){
+    public function isParent()
+    {
     	return $this->isParent;
     }
 
-    public function isPostponed(){
+    public function isPostponed()
+    {
     	return $this->suppressed;
     }
 
-    public function isShutdown(){
+    public function isShutdown()
+    {
     	return $this->shutdown;
     }
 
-    public function isAlerted(){
+    public function isAlerted()
+    {
     	return $this->alert;
     }
 
-    public function isOverriden(){
+    public function isOverriden()
+    {
     	return $this->overriden;
     }
 
-    public static function getPostponed(){
+    public static function getPostponed()
+    {
     	return Valve::where('postponed','1')->get();
     }
 
-    public static function getShutdown(){
+    public static function getShutdown()
+    {
     	return Valve::where('shutdown', '1')->get();
     }
 
-    public static function getAlerted(){
+    public static function getAlerted()
+    {
     	return Valve::where('alert', '1')->get();
     }
 
-    public static function getOverriden(){
+    public static function getOverriden()
+    {
     	return Valve::where('overriden', '1')->get();
     }
 
-    public function valve_groups(){
-    	return $this->belongsToMany(ValveGroup::class, 'valve_to_group');
+    public static function getRootValves()
+    {
+        return Valve::where('parent_id', NULL)->get();
+    }
+
+    public static function getUngroupedValves()
+    {
+        $ids = DB::table('valve_to_group')->pluck('valve_id');
+        $valves = Valve::all()->keyBy('id');
+        foreach($ids as $id)
+            $valves->pull($id);
+        return $valves;
+    }
+
+    public function valve_groups()
+    {
+    	return $this->belongsToMany(ValveGroup::class, 'valve_to_group')->get();
     }
 }
--- umbrella-irrigation/app/ValveGroup.php
+++ umbrella-irrigation/app/ValveGroup.php
@@ -2,6 +2,7 @@
 namespace App;
 use Webpatser\Uuid\Uuid;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class ValveGroup extends Model
 {
@@ -14,8 +15,21 @@ class ValveGroup extends Model
     }
 
     public $incrementing = false;
-    public function valves()
+
+    //valve group-> id -> find all valves with the same group id
+    public function getChildValves()
     {
-    	return $this->belongsToMany(Valve::class, 'valve_to_group');
+    	return $this->belongsToMany(Valve::class, 'valve_to_group')->get();
     }
+
+    public static function getRootGroups()
+    {
+        return ValveGroup::where('parent_valve_group', NULL)->get();
+    }
+
+    public function getChildGroups()
+    {
+        return $this::where('parent_valve_group', $this->id)->get();
+    }
+
 }
