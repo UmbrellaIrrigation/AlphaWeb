39b5358990828323f044ec94da0e9ad22c7cda4f
Lemuel Dizon <lemuel.dizon.703@my.csun.edu>
2017-11-29 22:59:05 -0800

    Testing tree

--- umb-front/resources/views/components/tree/users.blade.php
+++ umb-front/resources/views/components/tree/users.blade.php
@@ -1,4 +1,16 @@
 <aside class="col-md-2 offset-md-1 tree-nav">
     <h4 class="pl-3">Navigation Menu</h4>
     <hr>
+    <div id="tree">
+        <ul id="treeData" style="display: none;">
+            <li id="1">Node 1</li>
+            <li id="2" class="expanded folder">Folder 2
+                <ul>
+                    <li id="3">Node 2.1</li>
+                    <li id="4">Node 2.2</li>
+                </ul>
+            </li>
+            <li id="k234" class="lazy folder">This is a lazy loading folder with key k234.</li>
+        </ul>
+    </div>
 </aside>
--- umb-front/resources/views/layouts/master.blade.php
+++ umb-front/resources/views/layouts/master.blade.php
@@ -1,33 +1,46 @@
-
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
 
-    <title>Umbrella Irrigation</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="../../../../favicon.ico">
 
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  <title>Umbrella Irrigation</title>
 
-    <!-- Custom styles for this template -->
-    <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
-  </head>
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb"
+    crossorigin="anonymous">
 
-  <body>
-    @yield ('nav')
+  <!-- Custom styles for this template -->
+  <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
 
-    <div class="container-fluid">
-      <div class="row">
-        
-        @yield ('content')
-        
-      </div>
+  <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+  <link href="//cdn.jsdelivr.net/npm/jquery.fancytree@2.27/dist/skin-win8/ui.fancytree.min.css" rel="stylesheet">
+  <script src="//cdn.jsdelivr.net/npm/jquery.fancytree@2.27/dist/jquery.fancytree-all-deps.min.js"></script>
+
+  <script type="text/javascript">
+    $(function () {
+      $("#tree").fancytree();
+    });
+  </script>
+
+</head>
+
+<body>
+  @yield ('nav')
+
+  <div class="container-fluid">
+    <div class="row">
+
+      @yield ('content')
+
     </div>
+  </div>
 
-    @include ('components.footer')
-  </body>
+  @include ('components.footer')
+</body>
+
 </html>
