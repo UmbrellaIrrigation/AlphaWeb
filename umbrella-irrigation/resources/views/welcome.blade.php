8efe499d929386919fef716c765ecfc1383d3583
Lemuel Dizon <lemuel.dizon.703@my.csun.edu>
2017-12-06 15:07:29 -0800

    Welcome view user check

--- umbrella-irrigation/resources/views/welcome.blade.php
+++ umbrella-irrigation/resources/views/welcome.blade.php
@@ -6,16 +6,26 @@
 
 @section ('content')
     <section class="jumbotron jumbotron-fluid text-center">
-        <div class="container">
-            <h2 class="display-2">Welcome to Umbrella Irrigation</h2>
-            <hr>
-            <p class="lead text-muted">
-                To get started using Umbrella Irrigation, please login or register.
-            </p>
-            <div class="container col-5">
-                <a href="{{ route('login') }}" class="btn btn-primary btn-block btn-lg">Login</a>
-                <a href="{{ route('register') }}" class="btn btn-secondary btn-block btn-lg">Register</a>
+        @guest
+            <div class="container">
+                <h2 class="display-2">Welcome to Umbrella Irrigation</h2>
+                <hr>
+                <p class="lead text-muted">
+                    To get started using Umbrella Irrigation, please login or register.
+                </p>
+                <div class="container col-5">
+                    <a href="{{ route('login') }}" class="btn btn-primary btn-block btn-lg">Login</a>
+                    <a href="{{ route('register') }}" class="btn btn-secondary btn-block btn-lg">Register</a>
+                </div>
             </div>
-        </div>
+        @else
+            <div class="container">
+                <h2 class="display-2">Hello, {{ Auth::user()->name }}.</h2>
+                <hr>
+                <p class="lead text-muted">
+                    Please click on any section on the Navigation Menu to get started.
+                </p>
+            </div>
+        @endguest
     </section>
 @endsection
