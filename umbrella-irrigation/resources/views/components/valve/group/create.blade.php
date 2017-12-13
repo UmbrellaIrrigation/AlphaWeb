ad6b2e2c0de5ea078fb6ef3a8e08df65e4e3b39f
Lemuel Dizon <lemuel.dizon.703@my.csun.edu>
2017-12-13 10:16:40 -0800

    Added valves page

--- umbrella-irrigation/resources/views/components/valve/group/create.blade.php
+++ umbrella-irrigation/resources/views/components/valve/group/create.blade.php
@@ -1,20 +1,36 @@
-<div class="h1 text-center">Valve Group Form</div>
-<br>
-<form method="POST" action="/">
-	{{ csrf_field() }}
+<div class="modal fade" id="createValveGroupModal" tabindex="-1" role="dialog" aria-labelledby="createValveGroupModal" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
 
-	<div class="form-group">
-		<label for="name">Name:</label>
-		<input type="text" class="form-control" id="name" name="name" required>
+			<form method="POST" action="/">
+				{{ csrf_field() }}
+
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Create a New Valve Group</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+
+				<div class="modal-body">
+					<div class="form-group">
+						<label for="name">Name:</label>
+						<input type="text" class="form-control" id="name" name="name" required>
+					</div>
+
+					<div class="form-group">
+						<label for="parent_valve_gid">Parent Valve Group Id:</label>
+						<input type="number" class="form-control" id="parent_valve_gid" name="parent_valve_gid">
+					</div>
+				</div>
+
+				<div class="modal-footer">
+					<div class="form-group">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+						<button type="submit" class="btn btn-primary">Create Valve Group</button>
+					</div>
+				</div>
+			</form>
+		</div>
 	</div>
-
-	<div class="form-group">
-        <label for="parent_valve_gid">Parent Valve Group Id:</label>
-		<input type="number" class="form-control" id="parent_valve_gid" name="parent_valve_gid">
-	</div>
-
-	<div class="form-group">
-		<button type="submit" class="btn btn-primary">Create Valve Group</button>
-	</div>
-
-</form>
+</div>
---
+++ umbrella-irrigation/resources/views/components/valve/options.blade.php
@@ -0,0 +1,73 @@
+<nav class="col-md-1 d-none d-sm-block bg-light sidebar">
+    <ul class="nav nav-pills flex-column">
+        <li class="nav-item nav-link h5 pb-0">Valve Groups</li>
+        <li class="nav-item">
+            <a class="nav-link" href="#createValveGroupModal" data-toggle="modal" data-target="#createValveGroupModal">Create Group</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Delete Group</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Add to Group</a>
+        </li>
+    </ul>
+
+    <hr>
+
+    <ul class="nav nav-pills flex-column">
+        <li class="nav-item nav-link h5 pb-0">Valves</li>
+        <li class="nav-item">
+            <a class="nav-link" href="#createValveModal" data-toggle="modal" data-target="#createValveModal">Create Valve</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Delete Valve</a>
+        </li>
+    </ul>
+
+    <hr>
+
+    <ul class="nav nav-pills flex-column">
+        <li class="nav-item nav-link h5 pb-0">Filters</li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Suppressed
+                <span class="badge badge-primary">2</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Postponed
+                <span class="badge badge-primary">4</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Shutdown
+                <span class="badge badge-primary">6</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Alert
+                <span class="badge badge-primary">1</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Overridden
+                <span class="badge badge-primary">3</span>
+            </a>
+        </li>
+    </ul>
+
+    <hr>
+
+    <ul class="nav nav-pills flex-column">
+        <li class="nav-item nav-link h5 pb-0">Tree Options</li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" id="treeSort">Sort</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" id="treeExpand">Expand All</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" id="treeCollapse">Collapse All</a>
+        </li>
+    </ul>
+
+</nav>
