6abb0f7f2812a479b3fcf82a3df9f3f7d411098b
Lemuel Dizon <lemuel.dizon.703@my.csun.edu>
2017-12-13 00:47:26 -0800

    deleted base info

--- umbrella-irrigation/resources/views/components/user/tree.blade.php
+++ umbrella-irrigation/resources/views/components/user/tree.blade.php
@@ -22,37 +22,6 @@
                 </li>
             @endforeach
 
-            <li id="22" class="folder">Employees
-                <ul>
-                    <li id="6">Jeff Wiegley</li>
-                    <li id="7">Ramses Ordonez</li>
-                </ul>
-            </li>
-            <li id="231" class="folder">Admins
-                    <ul>
-                        <li id="2">
-                            <a href="/users/show" target="contentFrame">
-                                Lemuel Dizon
-                            </a>
-                        </li>
-                        <li id="1">
-                            <a href="//en.wikipedia.org/wiki/Snake" target="contentFrame">
-                                Raffi "Snake" Bilemjian
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-            <li id="3" class="folder">Guest
-                <ul>
-                    <li id="10" class="folder">Oviatt Library
-                        <ul>
-                            <li id="131">Sina "Belajio" Eradat</li>
-                        </ul>
-                    </li>
-                    <li id="411">John Doe</li>
-                    <li id="9">Jane Dow</li>
-                </ul>
-            </li>
         </ul>
     </div>
 </aside>
