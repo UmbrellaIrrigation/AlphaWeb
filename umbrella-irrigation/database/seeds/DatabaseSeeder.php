70e03c571a32184fddd2c25f51b650f264d94a5e
Unknown <rbilemjian@gmail.com>
2018-03-01 21:30:55 -0800

    Fixing Database seeder (added one neglected semicolon)

--- umbrella-irrigation/database/seeds/DatabaseSeeder.php
+++ umbrella-irrigation/database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(UserGroupTableSeeder::class);
-        $this->call(ValveTableSeeder::class)
+        $this->call(ValveTableSeeder::class);
     }
 }
