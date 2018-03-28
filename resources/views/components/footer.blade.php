dd9aba8651e1b9c5775332ecbf29713a6ee6bf65
Lemuel Dizon <lemuel.dizon.703@my.csun.edu>
2018-03-28 11:25:22 -0700

    Updated Dependencies

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,7 @@
 <!-- Bootstrap core JavaScript
     ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
+<script src="{{ URL::asset('js/vendor.js') }}"></script>
 <script src="{{ URL::asset('js/app.js') }}"></script>
 <script>
     var createModal = $('#createModal').modal('hide');
