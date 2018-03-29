14fcefc8e44a502dcebd1d25cb4c49c6ccb52326
Lemuel Dizon <lemuel.dizon.703@my.csun.edu>
2018-03-29 12:21:51 -0700

    Seperated User logic from valve logic

---
+++ resources/assets/js/pages/users.js
@@ -0,0 +1,72 @@
+/**
+ * Form and Vue Element
+ */
+
+Vue.component('example', require('../components/Example.vue'));
+
+const app = new Vue({
+    el: '#app',
+
+    components: {
+        Example
+    },
+
+    data: {
+        form: new Form({
+            name: '',
+            description: ''
+        })
+    },
+
+    methods: {
+        onSubmit() {
+            this.form.post('/projects')
+                .then(response => alert('Wahoo!'));
+        }
+    }
+});
+
+/**
+ * Fancytree Stuff
+ */
+$(function() {
+    var tree = $("#tree").fancytree({
+        checkbox: false,
+        debugLevel: 2,
+        minExpandLevel: 1,
+        postinit: function (isReloading, isError) {
+            this.reactivate();
+        },
+        focus: function (event, data) {
+            // Auto-activate focused node after 2 seconds
+            data.node.scheduleAction("activate", 2000);
+        },
+        activate: function (event, data) {
+            var node = data.node;
+            // Use <a> href and target attributes to load the content:
+            if (node.data.href) {
+                // Open target
+                window.open(node.data.href, node.data.target);
+                // or open target in iframe
+                //                $("[name=contentFrame]").attr("src", node.data.href);
+            }
+        }
+    });
+
+    $( "#treeSort" ).click(function() {
+        var node = tree.fancytree("getRootNode");
+        node.sortChildren(null, true);
+    });
+
+    $( "#treeExpand" ).click(function() {
+        tree.fancytree("getTree").visit(function(node){
+            node.setExpanded();
+        });
+    });
+
+    $( "#treeCollapse" ).click(function() {
+        tree.fancytree("getTree").visit(function(node){
+            node.setExpanded(false);
+        });
+    });
+});
